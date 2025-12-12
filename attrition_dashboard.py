ce50aa379d9adfc478866890378f00dedcaaa31b
Rach-fos <109030547+Rach-fos@users.noreply.github.com>
2025-12-12 14:41:40 -0500

    Update attrition_dashboard.py

--- attrition_dashboard.py
+++ attrition_dashboard.py
@@ -94,9 +94,14 @@ app.layout = dbc.Container([
                 id="year-filter",
                 min=MIN_YEAR, max=MAX_YEAR, step=1,
                 value=[MIN_YEAR, MAX_YEAR],
-                marks={str(y): str(y) for y in YEARS[::2]}  # Show every other year to save space
-            )
+                marks={y: {
+                        "label": str(y),
+                        "style": {"color": "#FFFFFF", "fontSize": "14px"}}
+                    for y in YEARS
+                },
+                tooltip={"placement": "bottom", "always_visible": False},
         ], width=6),
+        
         #filter 2
         dbc.Col([
             html.Label("Department"),
